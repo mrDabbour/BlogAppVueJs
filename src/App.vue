cecf5df77f0b171bd36b84b5cbeaad55def14e00
mrDabbour <jmomj@gmail.com>
2023-09-19 01:53:15 +0200

    create CardItem component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <h1>Hi , welcome in blog project</h1>
-  <blog-button title="Go there" background="crimson" color="#fff" />
-  <blog-button title="Go there" link="/blog" fullWidth />
+  <div>
+    <h1>Hi, welcome in blog project</h1>
+    <blog-button title="Go there" background="crimson" color="#fff" />
+    <blog-button title="Go there" link="/blog" fullWidth />
+    <card-item background="blue" :showMidea="true" :showHeader="true" :showActions="true">
+      <!-- هنا نقوم باستخدام الفتحة ونضع المحتوى الذي نريد عرضه -->
+         <template v-slot:media>
+        <h2>This is custom media content</h2>
+        <p>It can be anything you want.</p>
+      </template>
+    </card-item>
+    <card-item background="red"> </card-item>
+    <card-item></card-item>
+  </div>
 </template>
 
 <script>
 import BlogButton from "./components/BlogButton.vue";
+import CardItem from "./components/CardItem.vue";
+
 export default {
   name: "App",
-  components: { BlogButton },
+  components: { BlogButton, CardItem },
 };
 </script>
 
---
+++ src/components/CardItem.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="card" :style="{ background: background }">
+    <div v-show="showMedia" class="card-media">
+     <slot name="media"></slot> هنا نضيف الفتحة لعرض المحتوى المخصص
+    </div>
+
+    <div class="card-details">
+      <div v-show="showHeader" class="card-header">this is title</div>
+      <div v-show="showContent" class="card-content">this is content</div>
+      <div v-show="showActions" class="card-actions">Actions</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    background: {
+      type: String,
+      default() {
+        return "#fff";
+      },
+    },
+    showMedia: Boolean,
+    showHeader: Boolean,
+    showActions: Boolean,
+    showContent: {
+      type: Boolean,
+      default() {
+        return true;
+      },
+    },
+  },
+};
+</script>
+<style scoped>
+.card {
+  background: lightblue;
+  border: 2px solid #eee;
+  padding: 1rem;
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-around;
+  margin: 1rem auto;
+}
+
+.card:hover {
+  cursor: crosshair;
+  box-shadow: 1px 1px 1px 1px #eee;
+}
+.card-details {
+  margin: 0 1rem;
+}
+.card-header {
+  font-size: 2rem;
+  margin-bottom: 1.3rem;
+}
+
+.card-content {
+  font-size: 1.3rem;
+  margin: 1rem auto;
+}
+</style>

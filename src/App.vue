c295f0acc31279d27325516486e5109761272d6b
mrDabbour <jmomj@gmail.com>
2023-09-19 00:09:48 +0200

    create Blog button for the blog website to use it for all the project or website

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hi , welcome in blog project</h1>
+  <blog-button title="Go there" background="crimson" color="#fff" />
+  <blog-button title="Go there" link="/blog" fullWidth />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BlogButton from "./components/BlogButton.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { BlogButton },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
